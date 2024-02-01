6e2bf96225db67cb1ac5d165924cea1caf670d9b
Ahmad Maartmesrini <a17128753ahmed@gmail.com>
2024-02-01 09:33:14 +0300

    add data base

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require("express");
-const dataBase = [];
+const Datastore = require("nedb");
 
 const app = express();
 app.listen(3000, () => console.log("listening at 3000"));
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
+const database = new Datastore("database.db");
+database.loadDatabase();
+
 app.post("/api", (request, response) => {
-  console.log("I got a request!");
   const data = request.body;
-  dataBase.push(data);
-  response.json({
-    status: "success",
-    latitude: data.lat,
-    longitude: data.lon,
-  });
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  data.lat;
+  data.lon;
+  database.insert(data);
+  response.json(data);
 });
